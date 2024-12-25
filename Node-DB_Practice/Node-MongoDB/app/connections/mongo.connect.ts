772816392925c9d75d072988c98282c19af8c3ba
omkarmaliGit <omkarmali51@gmail.com>
2024-12-25 17:50:13 +0530

    connection with db

---
+++ Node-DB_Practice/Node-MongoDB/app/connections/mongo.connect.ts
@@ -0,0 +1,15 @@
+import { connect } from "mongoose";
+
+export const connectToMongo = async () => {
+  try {
+    const { MONGO_CONNECTION_STRING } = process.env;
+    console.log(MONGO_CONNECTION_STRING);
+    const data = await connect(MONGO_CONNECTION_STRING as string);
+    console.log(data);
+    console.log("Connected to MongoDB");
+    return true;
+  } catch (error) {
+    console.log(error);
+    throw "could not connect to mongo db";
+  }
+};
